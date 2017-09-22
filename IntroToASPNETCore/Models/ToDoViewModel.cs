c76f44260739fcb913906bfd3524179511b8caea
Gary Dix <gary.d@atcltd.atc.org.nz>
2017-09-22 15:18:21 +1200

    Initial setup

---
+++ IntroToASPNETCore/Models/ToDoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroToASPNETCore.Models
+{
+    public class ToDoViewModel
+    {
+        public IEnumerable<ToDoItem> Items
+        { get; set; }
+    }
+}
---
+++ IntroToASPNETCore/Services/FakeTodoItemService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntroToASPNETCore.Models;
+
+namespace IntroToASPNETCore.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<IEnumerable<ToDoItem>> GetIncompleteItemsAsync()
+        {
+            // Return an array of TodoItems
+            IEnumerable<ToDoItem> items = new[] {
+                new ToDoItem
+                {
+                    Title = "Learn ASP.NET Core",
+                    DueAt = DateTimeOffset.Now.AddDays(1)
+                },
+                new ToDoItem
+                {
+                    Title = "Build awesome apps",
+                    DueAt = DateTimeOffset.Now.AddDays(2)
+                }             };
+            return Task.FromResult(items);
+        }
+    }
+}
+
